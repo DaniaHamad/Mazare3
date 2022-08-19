ab821b53e5a7717ea3740781f4a1b493e8c43ba7
Ruqaya8 <104080597+Ruqaya8@users.noreply.github.com>
2022-08-19 16:33:11 +0300

    Update signup.dart

--- lib/views/signup.dart
+++ lib/views/signup.dart
@@ -67,8 +67,37 @@ class SignupView extends StatelessWidget {
                   color: Colors.grey,
                 ),
               ),
+               const SizedBox(
+                height: 6,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton.icon(
+                    onPressed: () {},
+                    icon: Icon(Icons.email),
+                    label: Text("  Gmail   "),
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.teal[300],
+                      textStyle: TextStyle(fontSize: 15),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 6,
+                  ),
+                  ElevatedButton.icon(
+                    onPressed: () {},
+                    icon: Icon(Icons.facebook),
+                    label: Text("Facebook"),
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.teal[300],
+                      textStyle: TextStyle(fontSize: 15),
+                    ),
+                  ),
+                ],
+              ),
               const SizedBox(
-                height: 12,
+                height: 6,
               ),
               Text(
                 'OR',
