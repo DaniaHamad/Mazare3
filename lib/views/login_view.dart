37e7f42bbd7f182342ed13404a0150fe110df7cf
Ruqaya8 <104080597+Ruqaya8@users.noreply.github.com>
2022-09-03 18:01:38 +0300

    Add files via upload

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'farmer_or_customer_view.dart';
-import 'signup_customer_view.dart';
+import 'login_view.dart';
+import 'login_with_view.dart';
 import 'signup_farmer_view.dart';
 
 class LoginView extends StatelessWidget {
@@ -41,7 +42,7 @@ class LoginView extends StatelessWidget {
             Icon(
               Icons.photo,
               color: Colors.grey.shade300,
-              size: 200,
+              size: 100,
             ),
             const SizedBox(
               height: 70,
@@ -157,7 +158,7 @@ class LoginView extends StatelessWidget {
                           onSurface: Colors.teal[300],
                         ),
                         onPressed: () {
-                          Get.to(LoginCustomerView());
+                          Get.to(LoginWithView());
                         },
                         child: const Text('Login?'),
                       ),
@@ -174,202 +175,6 @@ class LoginView extends StatelessWidget {
       ),
     );
   }
-}
-
-///////////////////////////////////////////////////////////////////////////////
-class LoginCustomerView extends StatelessWidget {
-  LoginCustomerView({Key? key}) : super(key: key);
-  TextEditingController emailCtrl = TextEditingController();
-  TextEditingController passCtrl = TextEditingController();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(25.0),
-        child: Column(
-          children: [
-            Row(
-              children: [
-                CustomPaint(
-                  size: Size(92, 92),
-                  painter: RPSCustomPainter(),
-                  child: IconButton(
-                    onPressed: () {
-                      Get.to(FarmerOrCustomerView());
-                    },
-                    icon: const Icon(Icons.keyboard_arrow_left_outlined),
-                    iconSize: 35,
-                    color: Colors.teal,
-                  ),
-                ),
-                const SizedBox(
-                  width: 30,
-                ),
-              ],
-            ),
-            Icon(
-              Icons.photo,
-              color: Colors.grey.shade300,
-              size: 100,
-            ),
-            const SizedBox(
-              height: 38,
-            ),
-            buildMyField(
-              'Example@gmail.com',
-              emailCtrl,
-              const Icon(Icons.email),
-              false,
-            ),
-            const SizedBox(height: 12),
-            buildMyField(
-              'Password',
-              passCtrl,
-              const Icon(Icons.security),
-              true,
-            ),
-            TextButton(
-              style: TextButton.styleFrom(
-                primary: Colors.grey,
-                onSurface: Colors.black,
-              ),
-              onPressed: () {},
-              child: const Text('forgot your password?'),
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            MaterialButton(
-                minWidth: 300,
-                height: 50,
-                color: Colors.teal.shade300,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(12.0)),
-                child: const Text(
-                  ' Log in ',
-                  style: TextStyle(
-                    fontSize: 20,
-                    color: Colors.white,
-                  ),
-                ),
-                onPressed: () {}),
-            const SizedBox(
-              height: 20,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: const [
-                Expanded(
-                  child: Divider(
-                    indent: 90.0,
-                    endIndent: 45.0,
-                    thickness: 1,
-                  ),
-                ),
-                Text(
-                  'OR',
-                  style: TextStyle(
-                    fontSize: 15,
-                    color: Colors.grey,
-                  ),
-                ),
-                Expanded(
-                  child: Divider(
-                    indent: 45.0,
-                    endIndent: 90.0,
-                    thickness: 1,
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.white,
-                    onPrimary: Colors.black,
-                  ),
-                  onPressed: () {},
-                  child: const Image(
-                    image: AssetImage("assets/images/google.png"),
-                    height: 22,
-                    width: 28,
-                  ),
-                ),
-                const SizedBox(
-                  width: 6,
-                ),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.white,
-                    onPrimary: Colors.black,
-                  ),
-                  onPressed: () {},
-                  child: const Image(
-                    image: AssetImage("assets/images/facebook.png"),
-                    height: 22,
-                    width: 28,
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 6,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text(
-                  'Do not have an account?',
-                  style: TextStyle(
-                    color: Colors.grey,
-                  ),
-                ),
-                TextButton(
-                  style: TextButton.styleFrom(
-                    primary: Colors.teal[300],
-                    onSurface: Colors.teal[300],
-                  ),
-                  onPressed: () {
-                    Get.to(SignupCustomerView());
-                  },
-                  child: const Text('sign up?'),
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-/*
-  @override
-  void dispose() {
-    super.dispose();
-    emailCtrl.dispose();
-    passCtrl.dispose();
-  }
-*/
-buildMyField(
-    String label, TextEditingController textCtrl, Icon myIcon, bool secure) {
-  return TextField(
-    controller: textCtrl,
-    decoration: InputDecoration(
-      prefixIcon: myIcon,
-      labelText: label,
-      border: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(18),
-      ),
-    ),
-    obscureText: secure,
-  );
 }
 
 class RPSCustomPainter extends CustomPainter {
