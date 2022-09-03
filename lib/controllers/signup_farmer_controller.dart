21d4266a9e837eb4d7bed7d4196375933c134e5b
Alaa <alaaissa649@gmail.com>
2022-09-04 00:33:39 +0300

    -image picker part 1

---
+++ lib/controllers/signup_farmer_controller.dart
@@ -0,0 +1,24 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
+
+class SignUpFarmerController extends GetxController {
+  var selectedImagePath = ''.obs;
+  var selectedImageSize = ''.obs;
+
+  void getImage(ImageSource imageSource) async {
+    final pickedFile = await ImagePicker().getImage(source: imageSource);
+    if (pickedFile != null) {
+      selectedImagePath.value = pickedFile.path;
+      selectedImageSize.value =
+          "${((File(selectedImagePath.value)).lengthSync() / 1024 / 1024).toStringAsFixed(2)}Mb";
+    } else {
+      Get.snackbar('Error', 'No Image selected !',
+          snackPosition: SnackPosition.BOTTOM,
+          backgroundColor: Colors.red,
+          colorText: Colors.white);
+    }
+  }
+}
